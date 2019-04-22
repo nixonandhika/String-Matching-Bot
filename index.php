e4586c29a55331777540d6ad744f402d19664455
hidx1 <nixonandhika28@gmail.com>
2019-04-22 08:48:01 +0700

    test

--- index.php
+++ index.php
@@ -7,37 +7,38 @@ use \LINE\LINEBot\SignatureValidator as SignatureValidator;
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-// initiate app
+// Slim app Initialization
 $configs =  [
 	'settings' => ['displayErrorDetails' => true],
 ];
 $app = new Slim\App($configs);
 
-/* ROUTES */
+//GET METHOD
 $app->get('/', function ($request, $response) {
 	return "Success";
 });
 
+//POST METHOD
 $app->post('/', function ($request, $response)
 {
-	// get request body and line signature header
+	// get request body and LINE signature
 	$body 	   = file_get_contents('php://input');
 	$signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
 
 	// log body and signature
 	file_put_contents('php://stderr', 'Body: '.$body);
 
-	// is LINE_SIGNATURE exists in request header?
-	if (empty($signature)){
-		return $response->withStatus(400, 'Signature not set');
-	}
+	// // Check LINE signature in request header
+	// if (empty($signature)){
+	// 	return $response->withStatus(400, 'Signature not set');
+	// }
+	//
+	// // Check Request Source
+	// if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, $_ENV['CHANNEL_SECRET'], $signature)){
+	// 	return $response->withStatus(400, 'Invalid signature');
+	// }
 
-	// is this request comes from LINE?
-	if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, $_ENV['CHANNEL_SECRET'], $signature)){
-		return $response->withStatus(400, 'Invalid signature');
-	}
-
-	// init bot
+	// Create bot client instance
 	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
 	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
 	$data = json_decode($body, true);
@@ -54,8 +55,8 @@ $app->post('/', function ($request, $response)
 			$reply = str_replace('"', "", $reply);
 		}
 		$reply = str_replace("<BR>", "\n", $reply);
-    $msgbuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($reply);
-		$res = $bot->replyMessage($event['replyToken'], $msgbuilder);
+        $msgbuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($reply);
+		$res = $bot->replyMessage($event['replyToken'], $msgbuilder); //Call API
 		return $res->getHTTPStatus() . ' ' . $res->getRawBody();
 	}
 });
